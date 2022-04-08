daa6d3ade9f79431bf231bde6d56ad61cd0fc9e4
Kenneth Leine Schulstad <ks@udp.no>
2022-04-08 09:59:19 +0200

    Add doctor

--- Makefile
+++ Makefile
@@ -1,14 +1,18 @@
+.PHONY: help
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
 .PHONY: default
 default: | help
+
+.PHONY: doctor
+doctor: ## Sanity check of required tools and environment settings
+	@./bin/doctor.sh
 
 .PHONY: update-all
 update-all: ## Clone or pull all changes from Dapla Team IaC repos
 	@./bin/git-update.sh
 
 .PHONY: tf-module-versions-current
-tf-module-versions-current: ## TODO
+tf-module-versions-current: ## For each IaC repo, list all tf modules and versions
 	@./bin/tf-module-versions-current.sh
-
-.PHONY: help
-help:
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
---
+++ bin/doctor.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Sanity check of required tools and environment settings
+# This script exits with status code=0 if everything is aye-okay
+source "$(dirname "$0")/_support-functions.sh"
+
+function validate_localstack_env {
+    validate_silently "make --version" "make is not installed" || return 1
+    validate_silently "jq --version" "jq is not installed" || return 1
+    validate_silently "terraform-module-versions --help" "terraform-module-versions is not installed" || return 1
+    validate_silently "go version" "go is not installed" || return 1
+    #validate_env GITHUB_TOKEN "Should hold your github auth token" || return 1
+
+    echo "Hooray! You seem to be $(green healthy)"
+}
+
+if [ -z $SKIP_EXEC ]
+then
+    validate_localstack_env
+fi
